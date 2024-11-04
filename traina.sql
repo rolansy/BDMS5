76a797a389109c882eee4c0c2a2f5ef5000869ce
rolansy <ronalshoeygeorge@gmail.com>
2024-11-05 04:59:50 +0530

    Add SQL scripts for reservation table creation, triggers, and procedures for making and cancelling reservations

--- traina.sql
+++ traina.sql
@@ -6,6 +6,14 @@ CREATE  TABLE Train (
     Seats NUMBER
 );
 
+CREATE TABLE Reservation (
+    ResID NUMBER PRIMARY KEY,
+    TrainNo NUMBER,
+    PassengerName VARCHAR2(15),
+    SeatNo NUMBER,
+    FOREIGN KEY (TrainNo) REFERENCES Railway(TrainNo)
+);
+
 create sequence train_seq
 start with 1
 increment by 1;
@@ -18,14 +26,61 @@ begin
 end;
 /
 
+create sequence res_seq
+start with 1
+increment by 1;
+
+create or replace trigger resid_trigger
+before insert on reservation
+for each ROW
+begin
+    select res_seq.NEXTVAL into :NEW.Resid from dual;
+end;
+/
+
+create or replace procedure makeres(
+    p_train in number,
+    p_name in varchar(20),
+    p_seat in number
+) as
+begin
+    insert into reservation(TrainNo,PassengerName,SeatNo)
+    values (p_train,p_name,p_seat);
+    commit;
+    DBMS_OUTPUT.PUT_LINE('Reservation made successfully for ' || p_PassengerName);
+    exception
+    when others then
+        rollback;
+        DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
+end;
+
+create or replace procedure cancelres(
+    p_res in number
+) as
+begin
+    delete from reservation where ResID=p_res;
+    commit;
+    DBMS_OUTPUT.PUT_LINE('Reservation cancelled successfully for ResID ' || p_ResID);
+EXCEPTION
+    WHEN OTHERS THEN
+        ROLLBACK;
+        DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
+end;
+/
+
+
 INSERT INTO Train (TName, TDate, Destination, Seats) VALUES ('Express', TO_DATE('2023-10-01', 'YYYY-MM-DD'), 'Delhi', 100);
 
 INSERT INTO Train (TName, TDate, Destination, Seats) VALUES ('Nirvana', TO_DATE('2023-10-02', 'YYYY-MM-DD'), 'Doha', 150);
-commit;
+COMMIT;
 
 INSERT INTO Train (TName, TDate, Destination, Seats) VALUES ('Vazha', TO_DATE('2023-10-02', 'YYYY-MM-DD'), 'Shimla', 150);
 ROLLBACK;
 
 
+SELECT * FROM Train;
 
-select * from train;
+
+begin
+    makeres(p_train=>1,p_name=>'John',p_seat=>2);
+end;
